6536192283c8b4446f403637b1514a2a790f1199
Destinykami <kamiqwer000@gmail.com>
2024-04-24 17:49:35 +0800

    63/110

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,29 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // Update goals scored and conceded for team 1
+        if !scores.contains_key(&team_1_name) {
+            // 创建新用户 都初始化为0
+            scores.insert(team_1_name.clone(),Team{
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+        };
+        if !scores.contains_key(&team_2_name) {
+            // 创建新用户 都初始化为0
+            scores.insert(team_2_name.clone(),Team{
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+        };
+        let team1=scores.get_mut(&team_1_name).unwrap();
+        team1.goals_scored+=team_1_score;
+        team1.goals_conceded+=team_2_score;
+ 
+        let team2=scores.get_mut(&team_2_name).unwrap();
+        team2.goals_scored+=team_2_score;
+        team2.goals_conceded+=team_1_score;
+
     }
     scores
 }
